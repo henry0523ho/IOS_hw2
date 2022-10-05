1565669e8be5562aa494ca748e836ba9292a838f
Henry <henry0523ho.3@gmail.com>
2022-10-05 17:06:18 +0800

    Initial Commit

---
+++ Shared/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  Shared
+//
+//  Created by henry on 2022/10/5.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

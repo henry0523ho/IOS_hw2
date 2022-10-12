3b33f32fcc2a81579eb0d067608e2df14081ba5d
Henry <henry0523ho.3@gmail.com>
2022-10-12 17:00:03 +0800

    test

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name=2;
+    @State private var posX=100;
+    @State private var posY=100;
+    @State private var show=false;
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Image(systemName: "die.face.\(name).fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width:200,height: 200)
+                .position(x:100 ,y:100)
+                .onTapGesture {
+                    name=Int.random(in: 1...6)
+                    posX=Int.random(in: 100...1000)
+                    posY=Int.random(in: 100...1000)
+                }
+            if show {
+                Image(systemName: "clock")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:200,height: 200)
+                    .transition(.opacity)
+                
+            }
+        }.onAppear{
+            withAnimation(.easeInOut(duration:5)){
+                show=true
+            }
+        }
+        
     }
 }
 

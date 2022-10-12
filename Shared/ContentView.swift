08fec68edefa0d6776a9e4963998ea6505be0540
Henry <henry0523ho.3@gmail.com>
2022-10-12 21:07:14 +0800

    202210240141

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,37 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name=2;
-    @State private var posX=100;
-    @State private var posY=100;
-    @State private var show=false;
     var body: some View {
+        NavigationView{
+            ZStack{
+                VStack{
+                    Text("hello")
+                    PageIcon(name:"hi", img:"fuck")
+                }.navigationTitle("俄羅斯方塊")
+            }
+            
+        }
+    }
+}
+
+struct PageIcon: View{
+    let name:String
+    let img:String
+    var body: some View{
         VStack{
-            Image(systemName: "die.face.\(name).fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width:200,height: 200)
-                .position(x:100 ,y:100)
-                .onTapGesture {
-                    name=Int.random(in: 1...6)
-                    posX=Int.random(in: 100...1000)
-                    posY=Int.random(in: 100...1000)
-                }
-            if show {
-                Image(systemName: "clock")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:200,height: 200)
-                    .transition(.opacity)
-                
-            }
-        }.onAppear{
-            withAnimation(.easeInOut(duration:5)){
-                show=true
-            }
+            Text(name)
         }
         
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
